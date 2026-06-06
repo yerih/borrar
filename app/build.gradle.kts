ab62b8607b33d48b979f40612c67308e6a962bab
Yerih <yerih7tup@gmail.com>
2026-06-06 19:35:48 +0200

    version updated to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mivuelto"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     buildTypes {
